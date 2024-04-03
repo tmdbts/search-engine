0bcb57aa4b5fb730f25bae015dee4156c43fb806
JoaoQuerido <tomas.querido@gmail.com>
2024-04-03 19:56:59 +0100

    Protocol connection in Storage Barrels

--- src/main/java/pt/uc/dei/student/tmdbts/search_engine/storage_barrels/StorageBarrelsImpl.java
+++ src/main/java/pt/uc/dei/student/tmdbts/search_engine/storage_barrels/StorageBarrelsImpl.java
@@ -2,22 +2,59 @@ package pt.uc.dei.student.tmdbts.search_engine.storage_barrels;
 
 import pt.uc.dei.student.tmdbts.search_engine.gateway.Gateway;
 import pt.uc.dei.student.tmdbts.search_engine.gateway.GatewayCallback;
+import pt.uc.dei.student.tmdbts.search_engine.protocol.SEProtocol;
+
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class StorageBarrelsImpl extends UnicastRemoteObject implements StorageBarrels, GatewayCallback {
+
+    private SEProtocol protocol;
+
     StorageBarrelsImpl() throws RemoteException{
         super();
+        try {
+            protocol = new SEProtocol();
+            startListening();
+        } catch (Exception e){
+            System.err.println("Error initializing multicast protocol: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void startListening(){
+        Thread listenerThread = new Thread(new MessageListener());
+        listenerThread.start();
+    }
+
+    private class MessageListener implements Runnable{
+        public void run(){
+            try {
+                while (true){
+                    String message = protocol.receiveMessage();
+                    System.out.println("Recived message: " + message);
+                }
+            } catch (Exception e){
+                System.out.println("Error while listening for multicast messages: " + e);
+                e.printStackTrace();
+            }
+        }
     }
 
     public void printOnBarrel(String s) throws RemoteException{
         System.out.println("> " +  s);
     }
 
+    public void writeOnFile() throws RemoteException{
+
+    }
+
     @Override
     public void search(String query) throws RemoteException {
+
         return;
     }
 
@@ -44,7 +81,7 @@ public class StorageBarrelsImpl extends UnicastRemoteObject implements StorageBa
 
         }
         catch (Exception e){
-            System.out.println("Exxeption in main Barrels: " + e);
+            System.out.println("Exception in main Barrels: " + e);
         }
     }
 }
