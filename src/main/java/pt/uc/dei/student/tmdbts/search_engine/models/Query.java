64e7c8b0dc82d512bd8fdfbeb6b6fa1bd9a2f49c
JoaoQuerido <105788009+JoaoQuerido@users.noreply.github.com>
2024-05-18 13:11:58 +0100

    TODO:Paging (#40)

--- src/main/java/pt/uc/dei/student/tmdbts/search_engine/models/Query.java
+++ src/main/java/pt/uc/dei/student/tmdbts/search_engine/models/Query.java
@@ -2,10 +2,12 @@ package pt.uc.dei.student.tmdbts.search_engine.models;
 
 
 import java.io.Serializable;
+import java.net.URI;
 
 public class Query implements Serializable {
 
     private String query;
+    private URI url;
 
     public void setQuery(String query) {
         this.query = query;
@@ -13,5 +15,13 @@ public class Query implements Serializable {
 
     public String getQuery() {
         return query;
+    }
+
+    public void setUrl(URI url) {
+        this.url = url;
+    }
+
+    public URI getUrl() {
+        return url;
     }
 }
